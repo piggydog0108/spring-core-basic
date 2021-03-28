61a8af3655069126129995dec8fb4f648b35924d
Allen <fire0108@gmail.com>
2021-03-28 22:51:42 +0900

    Dependency Injection with Spring

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -2,42 +2,72 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class AppConfig {
 
+//순수 자바 코드
+//public class AppConfig {
+//
+////    public MemberService memberService() {
+////        return new MemberServiceImpl(new MemoryMemberRepository());
+////    }
+////
+////    public OrderService orderService(){
+////        return new OrderServiceImpl(
+////                new MemoryMemberRepository()
+////                , new FixDiscountPolicy());
+////    }
+//
+//
+//    //역할과 구현을 한눈에  Refactoring
 //    public MemberService memberService() {
-//        return new MemberServiceImpl(new MemoryMemberRepository());
+//        return new MemberServiceImpl(memberRepository());
+//    }
+//
+//    private MemoryMemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
 //    }
 //
 //    public OrderService orderService(){
-//        return new OrderServiceImpl(
-//                new MemoryMemberRepository()
-//                , new FixDiscountPolicy());
+//        return new OrderServiceImpl(memberRepository(), discountPolicy());
 //    }
+//
+//    public DiscountPolicy discountPolicy(){
+////        return new FixDiscountPolicy();
+//        return new RateDiscountPolicy();
+//    }
+//}
 
 
-    //역할과 구현을 한눈에  Refactoring
+@Configuration
+public class AppConfig {
+
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy();
+        return new RateDiscountPolicy();
     }
-
-
 
 }
